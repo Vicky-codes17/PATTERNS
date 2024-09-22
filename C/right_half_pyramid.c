84403022a9343146b46fa1eec336d2f9c316296c
Vicky-codes17 <rameshvignesh0706@gmail.com>
2024-09-22 12:56:06 +0530

    Update Readme.md

---
+++ C/right_half_pyramid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int rows,i,j;
+
+    printf("Enter the number of rows :");
+    scanf("%d",&rows);
+
+    //For Loop
+    for(i = 1; i<= rows; i++)
+    {
+        //Inner For Loop
+        for(j = 1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
