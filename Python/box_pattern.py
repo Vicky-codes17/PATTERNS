212414d22681bc8c8bdfacef33eb3367284713d0
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-03-20 15:08:06 +0530

    modified box_pattern.py

--- Python/box_pattern.py
+++ Python/box_pattern.py
@@ -1,6 +1,6 @@
 num_rows = 5
-def right_half_pyramid(rows):
+def box_pattern(rows):
     for i in range(num_rows,rows + 1):
         for j in range(rows-1):
             print("* "*i)
-right_half_pyramid(num_rows)
+box_pattern(num_rows)
